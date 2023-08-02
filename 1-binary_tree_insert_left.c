c9e01b1024bc9f362091366d1f4e86eeff93d6e2
Twayfrancis <tawaifrancis@gmail.com>
2023-08-02 12:06:42 -0700

    betty requirement

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_insert_left - insert a node as left-child of another node
+ * @parent: pointer to node to insert the right-child in
+ * @value: vvalue to store in the new node
+ *
+ * Return: pointer to created node or NULL on failure
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
